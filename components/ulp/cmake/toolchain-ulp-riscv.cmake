ec4678ae4dd43afcc5e46ef449c991959bf6ae30
Alfonso Acosta <fons@hey.com>
2024-07-07 17:20:12 +0200

    Build RISCV ULP code with -O3

--- components/ulp/cmake/toolchain-ulp-riscv.cmake
+++ components/ulp/cmake/toolchain-ulp-riscv.cmake
@@ -5,9 +5,9 @@ set(CMAKE_C_COMPILER "riscv32-esp-elf-gcc")
 set(CMAKE_CXX_COMPILER "riscv32-esp-elf-g++")
 set(CMAKE_ASM_COMPILER "riscv32-esp-elf-gcc")
 
-set(CMAKE_C_FLAGS "-Os -march=rv32imc_zicsr_zifencei -mdiv -fdata-sections -ffunction-sections"
+set(CMAKE_C_FLAGS "-O3 -march=rv32imc_zicsr_zifencei -mdiv -fdata-sections -ffunction-sections"
     CACHE STRING "C Compiler Base Flags")
-set(CMAKE_CXX_FLAGS "-Os -march=rv32imc_zicsr_zifencei -mdiv -fdata-sections -ffunction-sections"
+set(CMAKE_CXX_FLAGS "-O3 -march=rv32imc_zicsr_zifencei -mdiv -fdata-sections -ffunction-sections"
     CACHE STRING "C++ Compiler Base Flags")
 set(CMAKE_ASM_FLAGS "-march=rv32imc -x assembler-with-cpp"
     CACHE STRING "Assembler Base Flags")
